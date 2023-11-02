5028b3fd3d6633c0db08689467db31b086510efe
Chris Robles <christian.robles@aol.com>
2023-11-02 16:15:30 -0700

    chapter 5 symfonycasts charming devlopment in symfony 5

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -1,12 +1,27 @@
 <?php
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route; //only thing needed to enable annotation routes
 
 class QuestionController {
     //The Mismarauder's Tavern
+
+    /**
+     * @Route("/")
+     */
     public function homepage(): Response //build the page
     {
         //return a Symfony Response object (Symfony\Component\HttpFoundation
         return new Response("What a bewitching controller we have conjured!");
+    }
+
+    /**
+     * @Route("/questions/{question}")
+     */
+    public function show($question): Response
+    {
+        $question = ucwords(str_replace('-', ' ', $question));
+        $question .= '?';
+        return new Response($question);
     }
 }
