091044304aaef501f117be7001a8fbb5a1be44d3
chris <christian.robles@aol.com>
2023-11-05 16:18:37 -0800

    chapter 9 symfonycasts charming development in symfony 5

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -1,15 +1,18 @@
 <?php
 namespace App\Controller;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; //shortcut methods
 use Symfony\Component\Routing\Annotation\Route; //only thing needed to enable annotation routes
+use Symfony\Component\HttpFoundation\Response; //ALL CONTROLLERS MUST RETURN RESPONSE
 
-class QuestionController {
+
+class QuestionController extends AbstractController {
     //The Mismarauder's Tavern
-
+    
     /**
      * @Route("/")
+     * Build the home page
      */
-    public function homepage(): Response //build the page
+    public function homepage(): Response
     {
         //return a Symfony Response object (Symfony\Component\HttpFoundation
         return new Response("What a bewitching controller we have conjured!");
@@ -17,11 +20,20 @@ class QuestionController {
 
     /**
      * @Route("/questions/{question}")
+     * Build the question page
      */
+
     public function show($question): Response
     {
-        $question = ucwords(str_replace('-', ' ', $question));
-        $question .= '?';
-        return new Response($question);
+        $question = ucwords(str_replace('-', ' ', $question)) . '?';
+        $answers = [
+            'Make sure your cat is sitting purrrfectly still ?',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe... try saying the spell backwards?',
+        ];
+        return $this->render('question/show.html.twig', [
+            'question' => $question,
+            'answers' => $answers
+        ]);
     }
 }
