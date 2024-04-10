27ec652e503e1d3feb508fc7e2c44df37ad2fabc
christian <christian@lv.net>
2024-04-10 13:18:20 -0700

    we are ROCKIGN thsi Symfony thing

--- cauldron_overflow/src/Controller/QuestionController.php
+++ cauldron_overflow/src/Controller/QuestionController.php
@@ -3,11 +3,24 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 
 class QuestionController
 {
+    /**
+     * @Route("/")
+     */
     public function homepage()
     {
         return new Response("What a bewitching controller we have conjured!");
+    }
+
+    /**
+     * @Route("/questions/{question}")
+     */
+    public function show($question)
+    {
+        return new Response(sprintf("Future page to show the question \"%s\"!", ucwords(str_replace("-", " ", $question))));
     }
 }
